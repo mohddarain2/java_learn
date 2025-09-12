ebd11b6f03f4c6bb65fc063a74a4855ac44548b9
MohammadDarain <mohddarain2@gmail.com>
2025-09-12 17:38:35 +0530

    Assignment_Operator.java

---
+++ Assignment_Operator.java
@@ -0,0 +1,16 @@
+public class Assignment_Operator {
+
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+
+        a=a+b; //30
+        a+=b; // 
+
+        System.out.println(a);
+        int c= 300;
+        int d = 200;
+        c-=d;
+        System.out.println(c);
+    }
+}
