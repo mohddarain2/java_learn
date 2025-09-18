16af754ba71b00b1bb579f2e897978403b38a620
MohammadDarain <mohddarain2@gmail.com>
2025-09-18 14:41:01 +0530

    Nested_if_else_conditions

--- Nestedifelse.java
+++ Nestedifelse.java
@@ -1,16 +1,16 @@
 public class Nestedifelse {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Nested_If_Else");
-        int age =18;
+        int age = 18;
         String gender = "Female";
-        if(age>=18){
+        if (age >= 18) {
             System.out.println("You are eligible");
-            if(gender !="Female"){
+            if (gender != "Female") {
                 System.out.println("You are invited");
-            }else{
-                System.out.println("You are not invited");
+            } else {
+                System.out.println("You are not invitted");
             }
-        }else{
+        } else {
             System.out.println("You are not eligible");
         }
     }
