179ee5525299ec3437c844062fabb247a16143c3
MohammadDarain <mohddarain2@gmail.com>
2025-09-18 14:16:50 +0530

    sc.close() syntax and conditions

---
+++ Conditions.java
@@ -0,0 +1,22 @@
+public class Conditions {
+
+    public static void main(String[] args) {
+        int a = 100;
+        if (a == 10) {
+            System.out.println("Yes you are right");
+        } else if (a == 100) {
+            System.out.println("Second case is right");
+        } else {
+            System.out.println("Try again");
+        }
+
+        String Name = "Mohammad Darain";
+        if (Name == "Mohammad Darain") {
+            System.out.println("Yes you are right");
+        } else if (Name == "Mohammad Faisal") {
+            System.out.println("Second case is right");
+        } else {
+            System.out.println("Try again");
+        }
+    }
+}
--- InputScanner.java
+++ InputScanner.java
@@ -1,27 +1,25 @@
 import java.util.Scanner;
-
 public class InputScanner {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // System.out.println("Please enter any number");100
         // Scanner sc= new Scanner(System.in);
         // int a = sc.nextInt();
         // System.out.println("You enter value is ");
         // System.out.println(a);
 
-        
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your name ");
-        String str = sc.nextLine();   
-        System.out.println("Please enter your age ");          
+        String str = sc.nextLine();
+        System.out.println("Please enter your age ");
         int age = sc.nextInt();
         System.out.println("Please enter your Salary ");
         Float salary = sc.nextFloat();
 
-        System.out.println("That  is Your name "+ str);
-        
-        System.out.println("That  is Your age "+ age);
-       
-        System.out.println("That  is Your Salary "+ salary);
+        System.out.println("That  is Your name " + str);
 
+        System.out.println("That  is Your age " + age);
+
+        System.out.println("That  is Your Salary " + salary);
+sc.close();
     }
 }
