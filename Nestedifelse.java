dbc5f01d45ea51c968331aac011f6d18ab81ac44
MohammadDarain <mohddarain2@gmail.com>
2025-09-18 14:36:07 +0530

    Nestedifelse_conditions

---
+++ Nestedifelse.java
@@ -0,0 +1,17 @@
+public class Nestedifelse {
+    public static void main(String[] args){
+        System.out.println("Nested_If_Else");
+        int age =18;
+        String gender = "Female";
+        if(age>=18){
+            System.out.println("You are eligible");
+            if(gender !="Female"){
+                System.out.println("You are invited");
+            }else{
+                System.out.println("You are not invited");
+            }
+        }else{
+            System.out.println("You are not eligible");
+        }
+    }
+}
