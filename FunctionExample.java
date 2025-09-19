112068fb0b079c1e0e48d50a14082133dc2b1c99
MohammadDarain <mohddarain2@gmail.com>
2025-09-19 17:18:23 +0530

    learn Functions Concept

---
+++ FunctionExample.java
@@ -0,0 +1,23 @@
+public class FunctionExample {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        //FunctionExample f1 = new FunctionExample();
+       // f1.getAge();
+       getAge();
+    }
+
+    // public int getAge(){
+    // System.out.println("Function called");
+    // return 20;
+    // }
+    // public void getAge(){
+    // System.out.println("Function called");
+
+    // }
+
+    static void getAge() {
+        System.out.println("Function called");
+       
+    }
+
+}
---
+++ FunctionExample2.java
@@ -0,0 +1,22 @@
+public class FunctionExample2 {
+    public static void main(String[] args) {
+        System.out.println("Functions");
+        //FunctionExample2 f1 = new FunctionExample2();
+        //f1.getAge();
+        getAge();
+    }
+
+    // public int getAge(){
+    // System.out.println("Function Called");
+    // return 20;
+    // }
+    // public void getAge() {
+    //     System.out.println("Function Called");
+    //     //return 20;
+    // }
+
+    static void getAge() {
+        System.out.println("Function Called");
+        //return 20;
+    }
+}
