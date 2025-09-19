5b2fb21ae7e35de9451b951010cf178460ff0312
MohammadDarain <mohddarain2@gmail.com>
2025-09-19 13:42:32 +0530

    Condition_With_Input

---
+++ ConditionWithInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConditionWithInput{
+    public static void main(String[] args){
+        System.out.println("Enter Your Age");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("Welcome to the club");
+        }else {
+            System.out.println("you are not allowed");
+        }
+    }
+}
