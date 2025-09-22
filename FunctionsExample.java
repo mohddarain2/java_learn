67d8b06e719b1366e306bea491f944fdec4aea36
MohammadDarain <mohddarain2@gmail.com>
2025-09-22 13:27:21 +0530

    practice Functions

---
+++ FunctionsExample.java
@@ -0,0 +1,18 @@
+public class FunctionsExample {
+
+    public static void main(String[] args){
+        // System.out.println("hello_Functions");
+    //    FunctionsExample fe = new FunctionsExample();
+    //    fe.sayHello();
+    sayHello(25);
+     sayHello(35);
+      sayHello(20);
+       sayHello(15);
+
+    }
+    public static void sayHello(int age){
+        //System.out.println("Hello From Fuction");
+        System.out.println(age*365);
+       
+    }
+}
