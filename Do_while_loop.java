74bd57dc7b8f9c4d7e74636353b67ef27c7f7467
MohammadDarain <mohddarain2@gmail.com>
2025-09-29 15:19:02 +0530

    Do_while_Loop

---
+++ Do_while_loop.java
@@ -0,0 +1,19 @@
+public class Do_while_loop {
+    public static void main(String[] args){
+        System.out.println("Do While Loop is Here");
+        // int item = 0;
+        // do{
+        //     System.out.println(item);
+        //     item++;
+
+        // }while(item<10);
+
+        int m=100;
+
+        do{
+            System.out.println(m);
+            // m=m-5;
+            m-=5;
+        }while(m>5);
+    }
+}
