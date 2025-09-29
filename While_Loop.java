7110d65117119384aedecfafefd2020c2f7230d2
MohammadDarain <mohddarain2@gmail.com>
2025-09-29 14:21:38 +0530

    Learn_while_loop

---
+++ While_Loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class While_Loop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int Num = sc.nextInt();
+        int data = 0;
+        
+        while (data <Num) { // 0<=20
+            data++; // 1 
+            if(data%2==0){ // 1%2==0
+
+                System.out.println(data); // 
+            }
+        }
+       //System.out.println(Num + " This value is large ");
+    }
+}
