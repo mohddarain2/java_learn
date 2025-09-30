d1b5f4ef7ff0c2d557cfa01618ca7c54381961e8
MohammadDarain <mohddarain2@gmail.com>
2025-09-30 14:03:29 +0530

    switch_problem

---
+++ Switch_case.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Switch_case {
+   
+    public static void main(String[] agrs){
+       
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt();
+        switch(age){
+            case 10: 
+            System.out.println("User age is 10");
+            break;
+            case 20 :
+            System.out.println("User Age is 20");
+            break;
+            case 30 : 
+            System.out.println("User Age is 30");
+            break;
+            default:System.out.println("Age Not matched");
+        }
+    }
+}
