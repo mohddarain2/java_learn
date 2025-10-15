322935b29bb759afd9f827eb49d4b53c05b79038
MohammadDarain <mohddarain2@gmail.com>
2025-10-15 16:26:16 +0530

    String_input_array

---
+++ Array_input_string_example.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Array_input_string_example {
+    public static void main(String[] args) {
+        System.out.println("Enter Number");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Names");
+       
+        String array[] = new String[number];
+        for(int i=0;i<number;i++){ // 0
+            array[i]=scan.nextLine();
+        }
+        System.out.println("Arrays here");
+
+        for(int i=0;i<number;i++){
+            System.out.println(array[i]);
+        }
+    }
+
+}
