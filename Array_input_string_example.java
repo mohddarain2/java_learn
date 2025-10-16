2cb4602e2a0078b6642d05403b47631d6a385d09
MohammadDarain <mohddarain2@gmail.com>
2025-10-16 10:01:52 +0530

    practice Arrays string input

--- Array_input_string_example.java
+++ Array_input_string_example.java
@@ -1,23 +1,45 @@
+// import java.util.Scanner;
+
+// public class Array_input_string_example {
+//     public static void main(String[] args) {
+//         System.out.println("Enter Number");
+//         Scanner sc = new Scanner(System.in);
+//         int number = sc.nextInt();
+        
+//         Scanner scan = new Scanner(System.in);
+//         System.out.println("Enter Names");
+       
+//         String array[] = new String[number];
+//         for(int i=0;i<number;i++){ // 0
+//             array[i]=scan.nextLine();
+//         }
+//         System.out.println("Arrays here");
+
+//         for(int i=0;i<number;i++){
+//             System.out.println(array[i]);
+//         }
+//     }
+
+// }
+
 import java.util.Scanner;
 
 public class Array_input_string_example {
-    public static void main(String[] args) {
+
+    public static void main(String args[]){
         System.out.println("Enter Number");
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Names");
-       
-        String array[] = new String[number];
-        for(int i=0;i<number;i++){ // 0
-            array[i]=scan.nextLine();
-        }
-        System.out.println("Arrays here");
+        int x = sc.nextInt();
 
-        for(int i=0;i<number;i++){
-            System.out.println(array[i]);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Arrays ");
+        String name[] = new String[x];
+        for(int i=0;i<x;i++){
+            name[i]=scan.nextLine();
+        }
+        System.out.println("Arrays Here");
+        for(int i=0;i<x;i++){
+            System.out.println(name[i]);
         }
     }
-
 }
