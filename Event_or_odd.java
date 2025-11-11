8e6ad0537cf03111b93d4cb1f704f92db3f93439
MohammadDarain <mohddarain2@gmail.com>
2025-11-11 11:04:01 +0530

    pracctice even or odd and 2d array

--- Event_or_odd.java
+++ Event_or_odd.java
@@ -8,3 +8,74 @@ public class Event_or_odd {
         }
     }
 }
+
+// -------------------------------------------------------------------------------------------
+
+// import java.util.Scanner;
+
+// public class Event_or_odd {
+
+//     public static void main(String[] args) {
+//         System.out.print("Pls Enter of row count: ");
+
+//         Scanner sc = new Scanner(System.in);
+//         int rows = sc.nextInt();
+
+//         System.out.print("Pls Enter of cols count: ");
+//         int cols = sc.nextInt();
+
+//         String array[][] = new String[rows][cols];
+//         System.out.println("Enter array Element:");
+
+//         sc.nextLine();
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+
+//                 System.out.print("Element of [" + i + "]+[" + j + "]:");
+//                 array[i][j] = sc.nextLine();
+//             }
+            
+//         }
+//         System.out.println("Element of array Print Here:");
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+
+//                 System.out.print(array[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class Event_or_odd {
+
+//     public static void main(String[] args){
+//         System.out.print("Please enter number of rows:");
+        
+//         Scanner sc = new Scanner(System.in);
+//         int rows = sc.nextInt();
+//         System.out.print("Please enter number of cols:");
+//         int cols = sc.nextInt();
+
+//         int array [][] = new int[rows][cols];
+//         System.out.println("Arrays Element are here:");
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+//                 System.out.print("Element is [" + i+"]["+j+"]:");
+//                 array[i][j]=sc.nextInt();
+//             }
+//             // System.out.println();
+//         }
+
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+                
+//                 System.out.print(array[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
